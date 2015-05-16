42e1ca9538032b83205c90213f69b306aee4f086
David Aaron Suddjian <aasuddjian@gmail.com>
2015-05-15 21:56:59 -0700

    Added a tests project

---
+++ KISS_Test/KISS_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KSP_KISS_Mod;
+
+namespace KISS_Test {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void Test() {
+
+		}
+	}
+}
---
+++ KSP_KISS_Mod/Tests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSP_KISS_Mod {
+	class Tests {
+	}
+}
